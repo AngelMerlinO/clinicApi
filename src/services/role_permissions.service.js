0732885fa5ce579213c9e8fec60c2e64c06cdbf5
Jose Angel Ortega Merlin <angelortegamerlin@gmail.com>
2025-07-04 18:53:59 -0600

    feat(roles): include permissions when fetching and saving roles
    - Eager-load Permissions in findAll and findById
    - Reload role with Permissions after create and update

--- src/services/role_permissions.service.js
+++ src/services/role_permissions.service.js
@@ -5,11 +5,11 @@ import { RolePermissions } from '../models/index.js';
 class RolePermissionsService extends BaseService {
   constructor() { super(RolePermissions); }
 
-  async create(data, opts)    { return super.create(data, opts); }
-  async findAll(opts)         { return super.findAll(opts);      }
-  async findById(id, opts)    { return super.findById(id, opts); }
-  async update(id, d, opts)   { return super.update(id, d, opts);}
-  async destroy(id, opts)     { return super.destroy(id, opts);  }
+  async create(data, opts)  { return super.create(data, opts); }
+  async findAll(opts)       { return super.findAll(opts);      }
+  async findById(id, opts)  { return super.findById(id, opts); }
+  async update(id, d, opts) { return super.update(id, d, opts);}
+  async destroy(id, opts)   { return super.destroy(id, opts);  }
 }
 
 export default new RolePermissionsService();
--- src/services/roles.service.js
+++ src/services/roles.service.js
@@ -1,16 +1,43 @@
 // api/src/services/roles.service.js
 import BaseService from './base.service.js';
-import { Roles }   from '../models/index.js';
+import { Roles, Permissions } from '../models/index.js';
 
 class RolesService extends BaseService {
-  constructor() { super(Roles); }
+  constructor() {
+    super(Roles);
+  }
 
-  // CRUD explícito  – puedes añadir validaciones extra si lo deseas
-  async create(data, opts)    { return super.create(data, opts); }
-  async findAll(opts)         { return super.findAll(opts);      }
-  async findById(id, opts)    { return super.findById(id, opts); }
-  async update(id, d, opts)   { return super.update(id, d, opts);}
-  async destroy(id, opts)     { return super.destroy(id, opts);  }
+  async create(data, opts = {}) {
+    const { permissions, permissionIds, permissionsIds, ...roleData } = data;
+    const permIds = permissions || permissionIds || permissionsIds || [];
+    const role = await super.create(roleData, opts);
+    if (Array.isArray(permIds) && permIds.length) {
+      await role.setPermissions(permIds, opts);
+    }
+    return role.reload({ include: [{ model: Permissions }] });
+  }
+
+  async findAll(opts = {}) {
+    return this.model.findAll({ include: [{ model: Permissions }], ...opts });
+  }
+
+  async findById(id, opts = {}) {
+    return this.model.findByPk(id, { include: [{ model: Permissions }], ...opts });
+  }
+
+  async update(id, data, opts = {}) {
+    const { permissions, permissionIds, permissionsIds, ...roleData } = data;
+    const permIds = permissions || permissionIds || permissionsIds || [];
+    const role = await super.update(id, roleData, opts);
+    if (Array.isArray(permIds)) {
+      await role.setPermissions(permIds, opts);
+    }
+    return role.reload({ include: [{ model: Permissions }] });
+  }
+
+  async destroy(id, opts = {}) {
+    return super.destroy(id, opts);
+  }
 }
 
 export default new RolesService();
